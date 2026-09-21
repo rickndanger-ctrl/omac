30fd3fdc3205545833929cceb35a3bf3c274719e
rickndanger-ctrl <rickndanger@gmail.com>
2026-09-21 01:10:02 -0700

    Support stable developer signing for Omac builds

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p bin
 /opt/homebrew/bin/python3 generate_config.py
 /usr/bin/swiftc Launcher.swift -o bin/AgentControlCenter -framework Cocoa -framework ApplicationServices -framework WebKit
 branding/screensaver/build.sh
-/usr/bin/codesign --force --sign - branding/screensaver/build/OMAC.saver
-/usr/bin/codesign --force --sign - branding/screensaver/build/OMAC-Preview.app
+/usr/bin/codesign --force --sign "${OMAC_SIGN_IDENTITY:--}" branding/screensaver/build/OMAC.saver
+/usr/bin/codesign --force --sign "${OMAC_SIGN_IDENTITY:--}" branding/screensaver/build/OMAC-Preview.app
 /opt/homebrew/bin/python3 -m unittest discover -p 'test_*.py'
 /usr/bin/plutil -lint launchd/*.plist
