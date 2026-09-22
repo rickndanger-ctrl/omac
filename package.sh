a2f9eb5fe0f2253c5410070bc5b13d2f1a4e254f
rickndanger-ctrl <rickndanger@gmail.com>
2026-09-22 12:06:02 -0700

    Version terminal focus fix as preview 1.2.2

--- package.sh
+++ package.sh
@@ -8,8 +8,8 @@ if [[ -z "$identity" ]]; then
  identity="$(security find-identity -v -p codesigning | sed -n 's/.*"\(Apple Development:.*\)"/\1/p' | head -n 1)"
 fi
 identity="${identity:--}"
-version=1.2.1-preview
-build_number=121
+version=1.2.2-preview
+build_number=122
 source_commit="$(git rev-parse HEAD)"
 arch="$(uname -m)"
 out="$PWD/dist"
